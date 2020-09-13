3e7a7e424aa2fba5252bf7a2eee67e1321d83779
Diego_Cruz_Rodriguez <alu0101105802@ull.edu.es>
2020-09-13 23:02:01 +0100

    LIADA

--- actor/include/actor.h
+++ actor/include/actor.h
@@ -9,11 +9,40 @@
 #include <thread>
 #include <pthread.h>
 #include <actors_global.h>
-#include <ActorManager.h>
 
 #include "Mailbox.h"
 #include "Delegate.h"
 
+class Actor;
+
+class EXPORTED ActorManager  {
+private:
+    std::unique_ptr<Actor>  root_actor_;
+public:
+    static  ActorManager* instance();
+
+    template<typename ActorClass>
+    ActorClass* spawn(Actor* parent = nullptr);
+
+    void kill(Actor* actor);
+
+    ~ActorManager();
+
+    template <typename... Types>
+    bool send( Actor* receiver , const std::string& message,Types&&... args);
+
+
+    static ActorManager* instance_;
+private:
+    ActorManager();
+
+};
+
+template<typename ActorClass>
+ActorClass *ActorManager::spawn(Actor *parent) {
+    static_assert(std::is_base_of<Actor,ActorClass>::value);
+    return new ActorClass(parent == nullptr ? root_actor_.get() : parent);
+}
 
 class EXPORTED Actor {
 private:
@@ -59,5 +88,59 @@ template<typename ActorClass>
 ActorClass *Actor::spawn() {
     return ActorManager::instance()->spawn<ActorClass>(this);
 }
+
+ActorManager* ActorManager::instance_ = nullptr;
+
+
+
+template<typename... Types>
+bool ActorManager::send(Actor *receiver, const std::string &message, Types &&... args) {
+    return receiver -> deliver_from(nullptr, message, std::forward<Types>(args)...);
+}
+
+
+template<typename... Types>
+bool Actor::reply(const Message& message,Types&&... args){
+    if(lastSender_){
+        return send(lastSender_ , message, std::forward<Types>(args)...);
+    }
+    return false;
+}
+
+
+
+template<typename... Types>
+bool Actor::send(Actor* receiver, const std::string& message, Types&&... args) {
+    return receiver->deliver_from(this, message, std::forward<Types>(args)...);
+}
+
+template<typename... Types>
+bool Actor::deliver_from(Actor *sender, const std::string &message, Types&&... args) {
+    // mailbox_.push(std::bind(invoke_handler<Types...>, message, args...));
+    mailbox_.push([this, message, args...]() {
+        this->invoke_handler(message, args...);
+    });
+    return true;
+}
+
+template<typename... Types>
+void Actor::invoke_handler(const std::string &message, Types &&... args) {
+    if (handlers_.count(message)){
+        auto delegate = handlers_.at(message);
+        delegate(std::forward<Types>(args)...);
+    } else {
+        // TODO: No exite el evento.
+        // TODO: Durante el desarrollo sería buena idea indicarlo con una
+        // TODO: advertencia en la consola.
+    }
+}
+
+
+template<typename... Types>
+void Actor::create_handler(const std::string &message, std::function<void(Types...)> fn) {
+    handlers_.emplace(message, fn);
+}
+
+
 
 #endif //SOA_1920_RASTREADOR_WEB_DIEGO_OSCAR_ACTOR_H
--- actor/src/actor.cpp
+++ actor/src/actor.cpp
@@ -1,62 +1,31 @@
 //
-// Created by darkwayc0de on 28/5/20.
+// Created by darkwayc0de on 13/9/20.
 //
+#include "actor.h"
+ActorManager::ActorManager(): root_actor_(new Actor(nullptr)){}
 
-#include <actor.h>
-#include <ActorManager.h>
-
-Actor::Actor(Actor* parent):
-                        thread_(&Actor::processMessage, this),
-                        parent_(parent){
+void ActorManager::kill(Actor* actor) {
 
 }
-template<typename... Types>
-bool Actor::reply(const Message& message,Types&&... args){
-    if(lastSender_){
-        return send(lastSender_ , message, std::forward<Types>(args)...);
-    }
-    return false;
+
+ActorManager::~ActorManager() {
+    root_actor_ -> deletelater();
 }
-
-void Actor::processMessage() {}
-
-template<typename... Types>
-bool Actor::send(Actor* receiver, const std::string& message, Types&&... args) {
-    return receiver->deliver_from(this, message, std::forward<Types>(args)...);
+Actor::~Actor() {
+    deletelater();
 }
-
-template<typename... Types>
-bool Actor::deliver_from(Actor *sender, const std::string &message, Types&&... args) {
-    // mailbox_.push(std::bind(invoke_handler<Types...>, message, args...));
-    mailbox_.push([this, message, args...]() {
-        this->invoke_handler(message, args...);
-    });
-    return true;
-}
-
-template<typename... Types>
-void Actor::invoke_handler(const std::string &message, Types &&... args) {
-    if (handlers_.count(message)){
-        auto delegate = handlers_.at(message);
-        delegate(std::forward<Types>(args)...);
-    } else {
-        // TODO: No exite el evento.
-        // TODO: Durante el desarrollo sería buena idea indicarlo con una
-        // TODO: advertencia en la consola.
-    }
-}
-
 void Actor::deletelater() {}
 
-template<typename... Types>
-void Actor::create_handler(const std::string &message, std::function<void(Types...)> fn) {
-    handlers_.emplace(message, fn);
+Actor::Actor(Actor* parent):
+        thread_(&Actor::processMessage, this),
+        parent_(parent){
+
 }
+void Actor::processMessage() {}
 
-Actor::~Actor() {
-   deletelater();
+ActorManager* ActorManager::instance() {
+    if(!instance_)
+        instance_ = new ActorManager();
+
+    return instance_;
 }
-
-
-
-
