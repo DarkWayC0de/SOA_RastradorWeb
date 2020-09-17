2d9c87f30c0a190dd6c4b3a96ef66f9e6b0659a2
Oscar Carrasco <oxcabe@gmail.com>
2020-09-17 10:14:08 +0100

    Enlazar librería gq para parsing de URIs

--- indexing-service/src/main.cpp
+++ indexing-service/src/main.cpp
@@ -4,17 +4,17 @@
 #include <iostream>
 #include <actor.h>
 #include "CurlFetcher.h"
-//#include "Document.h"
-//#include "Node.h"
+#include "Document.h"
+#include "Node.h"
 #include "HttpGetter.h"
 
 int main(int argc, char* argv[]){
     std::string page("<h1><a>some link</a></h1>");
-  //  CDocument doc;
-   // doc.parse(page.c_str());
+    CDocument doc;
+    doc.parse(page.c_str());
 
-  //  CSelection c = doc.find("h1 a");
- //   std::cout << c.nodeAt(0).text() << std::endl; // some link
+    CSelection c = doc.find("h1 a");
+    std::cout << c.nodeAt(0).text() << std::endl; // some link
 
     ActorManager::instance();
     auto getter = ActorManager::instance()->spawn<HttpGetter>();
