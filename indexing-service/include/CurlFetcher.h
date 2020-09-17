9cc1e85069e82f7f4c7eedd6851854ecd3ffe48a
Diego_Cruz_Rodriguez <alu0101105802@ull.edu.es>
2020-09-17 03:08:14 +0100

    Terminar clase CurlFetcher

--- indexing-service/include/CurlFetcher.h
+++ indexing-service/include/CurlFetcher.h
@@ -8,18 +8,23 @@
 
 #include <curl/curl.h>
 #include <atomic>
+#include <vector>
+#include <optional>
 
 class CurlFetcher {
 public:
+    struct FetchInfo{
+        std::vector<uint8_t> content;
+        std::string contentType;
+    };
     CurlFetcher();
 
     ~CurlFetcher();
 
-    void fetchURL(const std::string& url );
+     std::optional<FetchInfo> fetchURL(const std::string& url,long);
 
 private:
-    static std::atomic_int instanceCounnter_;
-    std::atomic_int instanceCounter_ = 0;
+    static std::atomic_int instanceCounter_;
 
     static size_t  curlWriteCallback(char *ptr, size_t size, size_t nmemb, void *userdata);
 
